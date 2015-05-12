1c7670013c929f6a22ff81e70b3b487f02cf7a54
inf101368 <inf101368@fh-wedel.de>
2015-05-12 15:23:18 +0200

    skizziert - live update von einem canvas ueber zeit

--- ray.py
+++ ray.py
@@ -12,6 +12,36 @@ class Circle:
 		self.y = y
 		self.r = radius
 
+
+class Window:
+	def update():
+		if geometry.contains(self.d[0], self.d[1]):
+			self.state[self.d[0]][self.d[1]] = "#000000"
+
+	def draw(self):
+		# calculate new state for self.d
+		self.update()
+
+		# next self.d
+		if (self.d[0] < self.dimension[0]:
+			self.d[0] = self.d[0] + 1
+		else:
+			self.d[0] = 0
+			self.d[1] = self.d[0] + 1
+
+		#draw state
+		for x,row in enumerate(self.state):
+			for y,value in enumerate(row):
+				img.put(value, (x, y))
+
+		self.after(1000, self.draw)
+
+	def __init__(self, calculate, dimension, geometry):
+		self.geometry = geometry
+		self.d = calculate
+		self.state = [["#ffffff" for x in range(dimension[0])] for x in range(dimension[1])]
+		self.dimension = dimension
+
 master = Tk()
 
 canvas = Canvas(master, width=200, height=200)
