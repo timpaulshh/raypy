fecb0af51af4ef044375af8484eeb4508249133f
inf101368 <inf101368@fh-wedel.de>
2015-05-12 14:56:12 +0200

    declare class; draw on canvas

--- ray.py
+++ ray.py
@@ -1,4 +1,16 @@
+#!/usr/bin/python
+
 from Tkinter import *
+import math
+
+class Circle:
+	def contains(self, x, y):
+		return  math.pow(x-self.x,2) + math.pow(y-self.y,2) <= math.pow(self.r,2)
+
+	def __init__(self, x, y, radius):
+		self.x = x
+		self.y = y
+		self.r = radius
 
 master = Tk()
 
@@ -9,6 +21,14 @@ img = PhotoImage(width=200, height=200)
 
 canvas.create_image((100, 100), image=img, state="normal")
 
-img.put("#ffffff", (10, 10))
+c = Circle(x=100, y=100, radius=50)
+
+for x in range(200):
+	for y in range(200):
+		if c.contains(x, y):
+			img.put("#000000", (x, y))
+
 
 mainloop()
+
+
