c9a6a520fe0b01bce44e2a88d18d18ff829717ab
mervyn.mccreight <mervyn.mccreight@gmail.com>
2015-05-12 18:30:56 +0200

    corrext syntacticxal errors

--- ray.py
+++ ray.py
@@ -14,8 +14,8 @@ class Circle:
 
 
 class Window:
-	def update():
-		if geometry.contains(self.d[0], self.d[1]):
+	def update(self):
+		if self.geometry.contains(self.d[0], self.d[1]):
 			self.state[self.d[0]][self.d[1]] = "#000000"
 
 	def draw(self):
@@ -23,42 +23,52 @@ class Window:
 		self.update()
 
 		# next self.d
+		# hier ist irgendwas falsch
 		if self.d[0] < self.dimension[0]:
 			self.d[0] = self.d[0] + 1
 		else:
-			self.d[0] = 0
-			self.d[1] = self.d[0] + 1
+			if self.d[1] < self.dimension[1]:
+				self.d[0] = 0
+				self.d[1] = self.d[1] + 1
+			else:
+				self.master.after_cancel(self.after_id)
+				return
 
 		#draw state
 		for x,row in enumerate(self.state):
 			for y,value in enumerate(row):
-				img.put(value, (x, y))
+				self.img.put(value, (x, y))
 
-		self.after(1000, self.draw)
+		# und zwar wenn man hier 10 statt 15 einsetzt?
+		self.after_id = self.master.after(15, self.draw)
 
-	def __init__(self, calculate, dimension, geometry):
+	def __init__(self, calculate, dimension, geometry, image, master):
 		self.geometry = geometry
 		self.d = calculate
 		self.state = [["#ffffff" for x in range(dimension[0])] for x in range(dimension[1])]
 		self.dimension = dimension
+		self.img = image
+		self.master = master
+		self.after_id = 0
 
 master = Tk()
 
-canvas = Canvas(master, width=200, height=200)
+WIDTH = 50
+HEIGHT = 50
+
+canvas = Canvas(master, width=WIDTH, height=HEIGHT)
 canvas.pack()
 
-img = PhotoImage(width=200, height=200)
+img = PhotoImage(width=WIDTH, height=HEIGHT)
 
-canvas.create_image((100, 100), image=img, state="normal")
+canvas.create_image((WIDTH / 2, HEIGHT / 2), image=img, state="normal")
 
-c = Circle(x=100, y=100, radius=50)
+c = Circle(x=WIDTH / 2, y=HEIGHT / 2, radius=WIDTH / 2)
 
-for x in range(200):
-	for y in range(200):
-		if c.contains(x, y):
-			img.put("#000000", (x, y))
+window = Window(calculate = [0,0], dimension = (WIDTH, HEIGHT), geometry = c, image = img, master = master)
 
+window.draw()
 
-mainloop()
+master.mainloop()
 
 
