bf9803be8a17fa0ae74dc5ba8b9f59cc003058e6
Tim Pauls <tim.pauls.hh@gmail.com>
2015-05-28 13:30:25 +0200

    Moved distance calculation to abstract RayTracer

--- tracer.py
+++ tracer.py
@@ -6,24 +6,21 @@ import numpy as np
 class RayTracer:
 	__metaclass__ = ABCMeta
 
-	@abstractmethod
-	def trace(self, ray, objects):
-		pass
-
-class SimpleRayTracer(RayTracer):
-	def __init__(self):
-		pass
-		
 	# returns a sorted list of tuples (distance, object)
-	def __distances(self, objects, ray):
+	def distances(self, objects, ray):
 		return sorted(list(self.__distancesgen(objects, ray)), key=lambda x: x[0])
 			
 	def __distancesgen(self, objects, ray):
 		for obj in objects:
 			yield (obj.intersect(ray), obj)
-	
+
+	@abstractmethod
 	def trace(self, ray, objects):
-		nearest = self.__distances(objects, ray)[0]
+		pass
+
+class SimpleRayTracer(RayTracer):
+	def trace(self, ray, objects):
+		nearest = self.distances(objects, ray)[0]
 		if min(nearest[0]) < np.inf:
 			return nearest[1].getColorHex()
 		else:
