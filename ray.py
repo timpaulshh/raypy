898fce92d637ae0f56e65c322bedea064f1415a0
mervyn.mccreight <inf101368@fh-wedel.de>
2015-06-05 18:25:18 +0200

    MOAR SPHERES!

--- ray.py
+++ ray.py
@@ -20,12 +20,14 @@ if __name__ == "__main__":
 	p6 = Plane([0, 0, -5], [0, 0, 1], Material(Color(200, 0, 0), 0.5, 0, 0.2))
 
 	s1 = Sphere([0, 3, 2], 2, Material(Color(200, 200, 0), 0.5, 0.8, 0.2))
-	s2 = Sphere([4, 2, 1], 0.5, Material(Color(150, 150, 150), 0.5, 0.8, 0.2))
+	s2 = Sphere([4, 2, 1], 0.5, Material(Color(0, 250, 0), 0.5, 0.8, 0.2))
+	s3 = Sphere([-3, 2, 1], 1, Material(Color(0, 0, 250), 0.5, 0.8, 0.2))
+	s4 = Sphere([2, -2, 1], 0.8, Material(Color(0, 250, 250), 0.5, 0.8, 0.2))
 
 	l1 = Sphere([0, -4, 0], 1, Material(Color(255, 255, 255), 0.5, 0.2, 0.2))
 
 	eye = [0, 0, -4.9]
 
-	scene = Scene(eye=eye, screen=screen, geometry=[s1, p1, p2, p3, p4, p5, p6, s2], lights=[l1])
+	scene = Scene(eye=eye, screen=screen, geometry=[s1, p1, p2, p3, p4, p5, p6, s2, s3, s4], lights=[l1])
 
 	window = Window(WIDTH, HEIGHT, scene, tracer=SimpleRayTracer())
