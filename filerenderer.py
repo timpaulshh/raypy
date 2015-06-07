0753237166ec1cc00d7a857e4aa79136db4f09eb
mervyn.mccreight <mervyn.mccreight@gmail.com>
2015-06-07 05:43:45 +0200

    fix process shit.

--- filerenderer.py
+++ filerenderer.py
@@ -12,46 +12,53 @@ class FileRenderer():
 		import sys
 		img = Image.new("RGB", (self.width, self.height))
 
-		from multiprocessing import Queue
-		q = Queue(maxsize=self.width * self.height)
-		q2 = Queue() # in this case yolo.
+		processcount = 4
 
-		threads = [BlockProcess(y * self.height/4, (y+1) * self.height/4, self.width, self.tracer, self.scene, q, q2) for y in range(4)]
+		from multiprocessing.queues import SimpleQueue
+		q2 = SimpleQueue() # in this case yolo.
+
+		threads = [BlockProcess(y * self.height/processcount, (y+1) * self.height/processcount, self.width, self.tracer, self.scene, q2) for y in range(processcount)]
 		count = len(threads)
+		print count
 
+		print "Starting Processes..."
 		for t in threads:
 			t.start()
+		print "Finished"
 
-		while not q.full():
-			import time
-			sys.stdout.write("Progress: %2.2f%%\r" % (q.qsize() / float(self.width*self.height) * 100))
-			sys.stdout.flush()
-			time.sleep(1)
-
-		print "Exit While."
-
-		for t in threads:
-			t.terminate()
-
+		count_finished = 0
+		drawn = 0
 		draw = ImageDraw.Draw(img)
 
-		while not q2.empty():
+		while count_finished < count:
 			info = q2.get()
-			draw.point(info[0], fill=info[1])
 
+			if info == "finished.":
+				count_finished = count_finished+1
+			else:
+				draw.point(info[0], fill=info[1])
+				drawn = drawn+1
+			
+			sys.stdout.write("Progress: %2.2f%%\r" % (drawn / float(self.width*self.height) * 100))
+			sys.stdout.flush()
+
+		print "Joining Processes..."
+		for e,t in enumerate(threads):
+			t.join()
+		print "Finished"
+			
 		img.save(fileName + ".png", format="png")
 
 import multiprocessing
 
 class BlockProcess(multiprocessing.Process):
-	def __init__(self, y_s, y_e, width, tracer, scene, queue, f_queue):
+	def __init__(self, y_s, y_e, width, tracer, scene, f_queue):
 		multiprocessing.Process.__init__(self)
 		self.y_s = y_s
 		self.y_e = y_e
 		self.tracer = tracer
 		self.scene = scene
 		self.width = width
-		self.queue = queue
 		self.f_queue = f_queue
 
 	def run(self):
@@ -59,7 +66,6 @@ class BlockProcess(multiprocessing.Process):
 			for x in range(self.width):
 				R = Ray.fromPoints(p1=self.scene.eye, p2=self.scene.screen.pixelToWorldCoord((x, y)))
 				C = self.tracer.trace(R, self.scene.geometry, self.scene.lights).toHex()
-				self.f_queue.put_nowait(((x,y), C))
-				self.queue.put_nowait(1)
-		print "Thread finished!"
+				self.f_queue.put(((x,y), C))
+		self.f_queue.put("finished.")
 
