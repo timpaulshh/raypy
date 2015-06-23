e4f4cbad5a64a4119af7e1a4753cb54060e65594
mervyn.mccreight <inf101368@fh-wedel.de>
2015-06-23 13:46:35 +0200

    multi processing

--- filerenderer.py
+++ filerenderer.py
@@ -11,14 +11,45 @@ class FileRenderer():
 	def render(self, fileName):
 		import sys
 		img = Image.new("RGB", (self.width, self.height))
-		draw = ImageDraw.Draw(img)
 
-		for x in range(self.width):
-			for y in range(self.height):
-				R = Ray.fromPoints(p1=self.scene.eye, p2=self.scene.screen.pixelToWorldCoord((x,y)))
-				C = self.tracer.trace(R, self.scene.geometry, self.scene.lights).toHex()
-				draw.point((x,y), fill=C)
-				sys.stdout.write("Progress: %2.2f%%\r" % ((x * float(self.height) + y)*100 / (self.width * self.height)))
-				sys.stdout.flush()
+		from multiprocessing import Queue
+		q = Queue(maxsize=self.width * self.height)
+
+		threads = [BlockProcess(y * self.height/4, (y+1) * self.height/4, self.width, self.tracer, self.scene, ImageDraw.Draw(img), q) for y in range(4)]
+
+		for t in threads:
+			t.start()
+
+		while not q.full():
+			import time
+			sys.stdout.write("Progress: %2.2f%%\r" % (q.qsize() / float(self.width*self.height) * 100))
+			sys.stdout.flush()
+			time.sleep(1)
+
+		for t in threads:
+			t.join()
 
 		img.save(fileName + ".png", format="png")
+
+
+import multiprocessing
+
+class BlockProcess(multiprocessing.Process):
+	def __init__(self, y_s, y_e, width, tracer, scene, draw, queue):
+		multiprocessing.Process.__init__(self)
+		self.y_s = y_s
+		self.y_e = y_e
+		self.tracer = tracer
+		self.scene = scene
+		self.draw = draw
+		self.width = width
+		self.queue = queue
+
+	def run(self):
+		for y in range(self.y_s, self.y_e):
+			for x in range(self.width):
+				R = Ray.fromPoints(p1=self.scene.eye, p2=self.scene.screen.pixelToWorldCoord((x, y)))
+				C = self.tracer.trace(R, self.scene.geometry, self.scene.lights).toHex()
+				self.draw.point((x, y), fill=C)
+				self.queue.put_nowait(1)
+
